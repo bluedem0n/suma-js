3083b236e090d3d7376f416c22f2490349567299
Sthefany Floriano <sthefanyflorianog@gmail.com>
2016-08-25 15:34:07 -0500

    Subiendo Ejercicio 1

---
+++ app.js
@@ -0,0 +1,4 @@
+var numero1 = prompt ("Sumando con Sthef : Ingresa el primer numero");
+var numero2 = prompt ("Sumando con Sthef : Ingresa el segundo numero");
+    var resultado = parseInt(numero1) + parseInt(numero2);
+    window.alert("Tu resultado es : " + resultado);
